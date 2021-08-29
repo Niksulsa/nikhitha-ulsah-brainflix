ad0ddbb8bee8fd4d167e2e5dea4b430701fb5139
Nikhitha Ulsah <87293732+Niksulsa@users.noreply.github.com>
2021-08-29 00:00:42 -0400

    Form created

--- src/pages/upload/UploadPage.js
+++ src/pages/upload/UploadPage.js
@@ -5,6 +5,36 @@ import {Link} from 'react-router-dom';
 import './UploadPage.scss';
 
 export default class UploadPage extends Component {
+    state={
+       
+    };
+
+    handleChange=(event)=>{
+        let formSubmit=this.formSubmitted();
+        console.log(formSubmit)
+        this.setState({
+            [event.target.title]:event.target.value
+        });
+    }
+
+    handlePublish=(event)=>{
+        event.preventDefault();
+
+        if(this.formSubmitted()){
+            alert("Your form is submiiteed")
+
+        }
+    }
+
+    formSubmitted=()=>{
+        if(
+            this.state.title && this.state.description
+        ){
+            return true;
+        }
+
+    }
+
     render() {
         return (
             <section className="upload">
@@ -18,14 +48,14 @@ export default class UploadPage extends Component {
                             alt=""/>
                     </div>
                     <section className="upload__formcontainer">
-                        <form className="upload__form">
+                        <form onSubmit={this.handlePublish} className="upload__form">
                             <div className="upload__titlebox">
                                 <label htmlFor="name" className="upload__title">TITLE YOUR VIDEO</label><br />
-                                <input type="text" className="upload__inputbox" name="name" placeholder="Add a title to your video"/>
+                                <input value={this.state.title} type="text" className="upload__inputbox" name="name" placeholder="Add a title to your video"/>
                             </div>
                             <div className="upload__description">
                                 <label htmlFor="textarea" className="upload__title" >ADD A VIDEO DESCRIPTION</label>
-                                <textarea name="comment" cols="35" rows="5" className="upload__videodescription" placeholder="Add a description to your video"></textarea>
+                                <textarea  value={this.state.description} name="comment" cols="35" rows="5" className="upload__videodescription" placeholder="Add a description to your video"></textarea>
                                 <div className="upload__buttonbox">
                                     <button id="cancel" type="reset" className="upload__cancel">CANCEL</button>
                                     <Link to="/" className="upload__publishbox">
